2edc600f2436fb2f8e2a3e2d8d72126aade9fd50
Joao Victor Piloni <jvpiloni@gmail.com>
2023-09-12 18:02:35 -0300

    Feature: Dialog List

--- src/app/shared/component/dialog-list/dialog-list.component.ts
+++ src/app/shared/component/dialog-list/dialog-list.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject, OnInit  } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { DetailFuncionarioComponent } from 'app/modules/admin/funcionario/detail-funcionario/detail-funcionario.component';
 import { Funcionario } from 'app/modules/admin/funcionario/shared/funcionario.model';
 import { FuncionarioService } from 'app/modules/admin/funcionario/shared/funcionario.service';
 import { Subject, debounceTime } from 'rxjs';
@@ -15,15 +17,29 @@ export class DialogListComponent implements OnInit {
   list: any[] = [];
   selectedItems: any[] = [];
   private debounceSubject = new Subject<string>();
+  listState: string;
 
 
   ngOnInit(): void {
     this.fetchData();
   }
 
-  constructor(public dialogRef: MatDialogRef <DialogListComponent>, @Inject(MAT_DIALOG_DATA) public data: { apiUrl: string, type: string }, private http: HttpClient, private funcionarioService: FuncionarioService) {
+
+  constructor(public dialogRef: MatDialogRef <DialogListComponent>,
+     @Inject(MAT_DIALOG_DATA) public data: { apiUrl: string, type: string, class: string },
+      private http: HttpClient, private funcionarioService: FuncionarioService,
+      private router: Router,
+      private dialog: MatDialog) {
     this.debounceSubject.pipe(debounceTime(500)).subscribe((valorDoInput) => {
       this.searchByName(valorDoInput);
+    });
+  }
+  
+  new(){
+    this.dialog.open(DetailFuncionarioComponent).afterClosed().subscribe((result) => {
+      this.fetchData();
+      this.listState = 'list';
+      this.dialogRef.updateSize('500px', '500px');
     });
   }
 
@@ -59,5 +75,10 @@ export class DialogListComponent implements OnInit {
       , error => {
         console.log(error);
       });
+  }
+
+  stateList(){
+    this.listState = 'list';
+    this.dialogRef.updateSize('500px', '500px');
   }
 }
